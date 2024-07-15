6df220341e66dd8caf16de62dbb45993c943fa4b
[chase friedman] <[chaseafriedman@gmail.com]>
2024-07-15 12:38:09 -0700

    added placeholder for shop page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "79f1ee29d6199f1b"; }
+/******/ 		__webpack_require__.h = function() { return "09010a750336dbdf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
