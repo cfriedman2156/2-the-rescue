847058a2a75880b963fce2aa4e44f6b4225d3b24
[chase friedman] <[chaseafriedman@gmail.com]>
2024-08-27 11:57:07 -0700

    fixing favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "db36bceadd1ae986"; }
+/******/ 		__webpack_require__.h = function() { return "3d34a4b74981f0d8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
