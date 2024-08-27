bc825e4073aba085ac673aae532649961b4bc62f
[chase friedman] <[chaseafriedman@gmail.com]>
2024-08-27 16:39:48 -0700

    fixed css bug on horses and donkeys pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "53e5d3089da81309"; }
+/******/ 		__webpack_require__.h = function() { return "6c0dd59b98f53823"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
